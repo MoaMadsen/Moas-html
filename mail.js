5c9ae452baf3d1bb6c429ba28248dfaa962fb0d9
MoaMadsen <teppattra@gmail.com>
2022-05-13 09:14:32 +0200

    uppdate pictures

---
+++ mail.js
@@ -0,0 +1,7 @@
+// function subcription() {
+//     let result="";
+// result += "<h3>Thank you for your subcribtion</h3>";
+// document.getElementsById("email").innerHTML = result;
+// }
+clickme = () => {
+    document.getElementById("email").innerHTML = "THANK YOU FOR YOUR SUBSCIPTION!";}
